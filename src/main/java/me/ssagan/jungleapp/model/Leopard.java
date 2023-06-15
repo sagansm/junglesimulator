1eaf6a31b96a91ccce59552b0a12ded5ae035967
Светлана <aminova.svekta@gmail,com>
2023-06-15 22:04:37 +0300

    add some events

--- src/main/java/me/ssagan/jungleapp/model/Leopard.java
+++ src/main/java/me/ssagan/jungleapp/model/Leopard.java
@@ -3,33 +3,25 @@ package me.ssagan.jungleapp.model;
 public class Leopard {
     private int health = 100;
     private int energy = 100;
-    private final double COEFFICIENT = 2.5;
+    public final double COEFFICIENT = 2.5;
 
-    public int getHealth(){
+    public int getHealth() {
         return health;
     }
 
-    public void setHealth(int health){
-        if (health > 100){
-            health = 100;
-        }
-        else if (health < 0){
-            health = 0;
-        }
+    public void setHealth(int health) {
+        if (health > 100) health = 100;
+        else if (health < 0) health = 0;
         this.health = health;
     }
 
-    public int getEnergy(){
+    public int getEnergy() {
         return energy;
     }
 
-    public void setEnergy(int energy){
-        if (energy > 100){
-            energy = 100;
-        }
-        else if (energy < 0){
-            energy = 0;
-        }
+    public void setEnergy(int energy) {
+        if (energy > 100) energy = 100;
+        else if (energy < 0) energy = 0;
         this.energy = energy;
     }
 }
--- src/main/java/me/ssagan/jungleapp/service/EventManager.java
+++ src/main/java/me/ssagan/jungleapp/service/EventManager.java
@@ -4,30 +4,49 @@ import me.ssagan.jungleapp.model.Leopard;
 
 public class EventManager {
 
-    public void startSimulation(Leopard leopard){
-        while(isAlive(leopard)){
+    public void startSimulation(Leopard leopard) {
+        while (isAlive(leopard)) {
             int randomNumber = (int) (Math.random() * 100);
-            if(randomNumber >= 0 && randomNumber < 100){
+
+            if (randomNumber >= 0 && randomNumber < 40) {
                 move(leopard);
+            } else if (randomNumber >= 40 && randomNumber < 60) {
+                sleep(leopard);
+            } else if (randomNumber >= 60 && randomNumber < 80) {
+                eatAntelope(leopard);
+            } else if (randomNumber >= 80 && randomNumber < 100) {
+                attackedByHunter(leopard);
             }
-            try{
-                Thread.sleep(500);
-            } catch (InterruptedException e){
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
                 throw new RuntimeException();
             }
         }
-        System.out.println("Leopard goes to heaven...");
     }
 
     //пробежал
-    private void move(Leopard leopard){
-        System.out.println("leo move");
+    private void move(Leopard leopard) {
         leopard.setEnergy(leopard.getEnergy() - 5);
+        System.out.print("leo move. ");
         correctHealth(leopard);
     }
 
     //поспал
-    //съел
+    private void sleep(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() + 20);
+        System.out.print("leo sleep. ");
+        correctHealth(leopard);
+    }
+
+    //съел Антилопу
+    private void eatAntelope(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 10);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 4));
+        System.out.print("leo ate Antelope. ");
+        correctHealth(leopard);
+    }
+
     //антилопами, - antelope
     //оленями, - deer
     //зебр - zebra
@@ -40,20 +59,26 @@ public class EventManager {
     //зайцев - hare
     //птицами, bird
 
+    private void attackedByHunter(Leopard leopard) {
+        leopard.setHealth(leopard.getHealth() - 20);
+        System.out.print("leo is attacked by hunter. ");
+        correctHealth(leopard);
+    }
+
     //а жив ли Леопард?
-    private boolean isAlive(Leopard leopard){
-        if(leopard.getHealth() <= 0){
+    private boolean isAlive(Leopard leopard) {
+        if (leopard.getHealth() <= 0) {
+            System.out.println("Leopard goes to heaven...");
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
 
-    private void correctHealth(Leopard leopard){
-        if (leopard.getEnergy() <= 0){
+    private void correctHealth(Leopard leopard) {
+        if (leopard.getEnergy() <= 0) {
             leopard.setHealth(leopard.getHealth() - 5);
-            System.out.println("Health: " + leopard.getHealth());
         }
+        System.out.println("Energy: " + leopard.getEnergy() + ". Health: " + leopard.getHealth());
     }
 }
