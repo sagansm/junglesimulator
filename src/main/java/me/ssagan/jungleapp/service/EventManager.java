4681fe81590f8538f9f7a0145f4c721b6c4751ef
Светлана <aminova.svekta@gmail,com>
2023-06-15 23:03:03 +0300

    add ten events

--- src/main/java/me/ssagan/jungleapp/service/EventManager.java
+++ src/main/java/me/ssagan/jungleapp/service/EventManager.java
@@ -3,18 +3,34 @@ package me.ssagan.jungleapp.service;
 import me.ssagan.jungleapp.model.Leopard;
 
 public class EventManager {
+    //оленями, - deer
+    //лошадей - horse
+    //кабана - boar
+
 
     public void startSimulation(Leopard leopard) {
         while (isAlive(leopard)) {
             int randomNumber = (int) (Math.random() * 100);
 
-            if (randomNumber >= 0 && randomNumber < 40) {
+            if (randomNumber >= 0 && randomNumber < 10) {
                 move(leopard);
-            } else if (randomNumber >= 40 && randomNumber < 60) {
+            } else if (randomNumber >= 10 && randomNumber < 20) {
                 sleep(leopard);
-            } else if (randomNumber >= 60 && randomNumber < 80) {
+            } else if (randomNumber >= 20 && randomNumber < 30) {
                 eatAntelope(leopard);
-            } else if (randomNumber >= 80 && randomNumber < 100) {
+            } else if (randomNumber >= 30 && randomNumber < 40) {
+                eatZebra(leopard);
+            } else if (randomNumber >= 40 && randomNumber < 50) {
+                eatMonkey(leopard);
+            } else if (randomNumber >= 50 && randomNumber < 60) {
+                eatWolf(leopard);
+            } else if (randomNumber >= 60 && randomNumber < 70) {
+                eatSheep(leopard);
+            } else if (randomNumber >= 70 && randomNumber < 80) {
+                eatHare(leopard);
+            } else if (randomNumber >= 80 && randomNumber < 90) {
+                eatBird(leopard);
+            } else if (randomNumber >= 90 && randomNumber < 100) {
                 attackedByHunter(leopard);
             }
             try {
@@ -41,23 +57,60 @@ public class EventManager {
 
     //съел Антилопу
     private void eatAntelope(Leopard leopard) {
-        leopard.setEnergy(leopard.getEnergy() - 10);
-        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 4));
+        leopard.setEnergy(leopard.getEnergy() - 20);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 7));
         System.out.print("leo ate Antelope. ");
         correctHealth(leopard);
     }
 
-    //антилопами, - antelope
-    //оленями, - deer
     //зебр - zebra
-    //лошадей - horse
-    //кабана - boar
+    private void eatZebra(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 18);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 6));
+        System.out.print("leo ate Zebra. ");
+        correctHealth(leopard);
+    }
+
     //обезьянами,  - monkey
+    private void eatMonkey(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 15);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 5));
+        System.out.print("leo ate Monkey. ");
+        correctHealth(leopard);
+    }
+
     //волков, - wolf
+    private void eatWolf(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 13);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 4));
+        System.out.print("leo ate Wolf. ");
+        correctHealth(leopard);
+    }
+
     //лисиц - fox
     //овец - sheep
+    private void eatSheep(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 10);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 3));
+        System.out.print("leo ate Sheep. ");
+        correctHealth(leopard);
+    }
+
     //зайцев - hare
+    private void eatHare(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 8);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 2));
+        System.out.print("leo ate Hare. ");
+        correctHealth(leopard);
+    }
+
     //птицами, bird
+    private void eatBird(Leopard leopard) {
+        leopard.setEnergy(leopard.getEnergy() - 6);
+        leopard.setHealth(leopard.getHealth() + (int) (leopard.COEFFICIENT * 1));
+        System.out.print("leo ate Bird. ");
+        correctHealth(leopard);
+    }
 
     private void attackedByHunter(Leopard leopard) {
         leopard.setHealth(leopard.getHealth() - 20);
