89b063637397a28000adb3eca9393fe381a2300e
Светлана <aminova.svekta@gmail,com>
2023-06-15 18:06:55 +0300

    add Leopard properties

--- src/main/java/me/ssagan/jungleapp/model/Leopard.java
+++ src/main/java/me/ssagan/jungleapp/model/Leopard.java
@@ -1,4 +1,23 @@
 package me.ssagan.jungleapp.model;
 
 public class Leopard {
+    private int health = 100;
+    private int energy = 100;
+    private final double COEFFICIENT = 2.5;
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public int getEnergy(){
+        return energy;
+    }
+
+    public void setEnergy(int energy){
+        this.energy = energy;
+    }
 }
